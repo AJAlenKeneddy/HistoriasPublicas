aa5b7c7ce3213c968a5cbd18c6ed1c3205ceac75
Alen Keneddy Aguilar Jara <alenaguilar24@gmail.com>
2024-09-11 17:44:58 -0500

    Implementacion del Consumo del Endpoint por el Servicio

---
+++ StoryBlazeServer/Services/CategoriaService.cs
@@ -0,0 +1,27 @@
+﻿using StoryBlazeServer.Models;
+
+namespace StoryBlazeServer.Services
+{
+    public class CategoriaService
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public CategoriaService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Categoria>> GetCategorias()
+        {
+            var response = await _httpClient.GetFromJsonAsync<Response<List<Categoria>>>("https://localhost:7184/api/Categoria/ListadoCategoria");
+
+            if (response != null && response.IsSuccess)
+            {
+                return response.Data;
+            }
+
+            return new List<Categoria>();
+        }
+    }
+}
